3770ee8c6c3a6f0ad17e489dff3cfadf4b15eddd
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-07-14 18:04:40 +0200

    Update files (have bugs)

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,44 +1,75 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Modal from '../Modal';
 import { Item, Image, ModalImage } from './ImageGalleryItem.styled';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-class ImageGalleryItem extends Component {
-  state = {
-    largeImageURL: null,
+const ImageGalleryItem = ({ image, largeImage }) => {
+
+  const [largeImageURL, setLargeImageURL] = useState(null);
+
+  const onOpenModal = image => {
+    setLargeImageURL(image);
   };
 
-  onOpenModal = image => {
-    this.setState({
-      largeImageURL: image,
-    });
+  const onCloseModal = () => {
+    setLargeImageURL(null);
   };
 
-  onCloseModal = () => {
-    this.setState({ largeImageURL: null });
-  };
 
-  render() {
-    const { largeImageURL } = this.state;
-    const { image, largeImage } = this.props;
-    return (
-      <>
-        <Item
-          onClick={() => {
-            this.onOpenModal(largeImage);
-          }}
-        >
-          <Image src={image} alt="" />
-        </Item>
-        {largeImageURL && (
-          <Modal onCloseModal={this.onCloseModal}>
-            <ModalImage src={largeImageURL} alt="" />
-          </Modal>
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Item
+        onClick={() => {
+          onOpenModal(largeImage);
+        }}
+      >
+        <Image src={image} alt="" />
+      </Item>
+      {largeImageURL && (
+        <Modal onCloseModal={onCloseModal}>
+          <ModalImage src={largeImageURL} alt="" />
+        </Modal>
+      )}
+    </>
+  );
+};
+
+// class ImageGalleryItem extends Component {
+//   state = {
+//     largeImageURL: null,
+//   };
+
+  // onOpenModal = image => {
+  //   this.setState({
+  //     largeImageURL: image,
+  //   });
+  // };
+
+  // onCloseModal = () => {
+  //   this.setState({ largeImageURL: null });
+  // };
+
+//   render() {
+//     const { largeImageURL } = this.state;
+//     const { image, largeImage } = this.props;
+//     return (
+// <>
+//   <Item
+//     onClick={() => {
+//       this.onOpenModal(largeImage);
+//     }}
+//   >
+//     <Image src={image} alt="" />
+//   </Item>
+//   {largeImageURL && (
+//     <Modal onCloseModal={this.onCloseModal}>
+//       <ModalImage src={largeImageURL} alt="" />
+//     </Modal>
+//   )}
+// </>
+//     );
+//   }
+// }
 
 ImageGalleryItem.propTypes = {
   image: PropTypes.string.isRequired,
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,36 +1,32 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, ModalBlock } from './Modal.styled';
 import PropTypes from 'prop-types';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount = () => {
-    window.addEventListener('keydown', this.onClose);
-  };
-
-  componentWillUnmount = () => {
-    window.removeEventListener('keydown', this.onClose);
-  };
-
-  onClose = e => {
-    const { onCloseModal } = this.props;
+const Modal = ({ onCloseModal, children }) => {
+  const onClose = e => {
     if (e.code === 'Escape' || e.currentTarget === e.target) {
       onCloseModal();
     }
   };
+  
+  useEffect(() => {
+    window.addEventListener('keydown', onClose);
 
-  render() {
-    const { children } = this.props;
-    return createPortal(
-      <Overlay onClick={this.onClose}>
-        <ModalBlock>{children}</ModalBlock>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+    return () => {
+      window.removeEventListener('keydown', onClose);
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <Overlay onClick={onClose}>
+      <ModalBlock>{children}</ModalBlock>
+    </Overlay>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
   onClose: PropTypes.func,
